8a4853e2037f36a4abc39d5bb9f36750b2060c5c
Bill Rocha <ahcordesign@gmail.com>
2023-07-31 15:03:17 -0300

    Add: Bytemd markdown editor

---
+++ src/routes/editor/+page.svelte
@@ -0,0 +1,36 @@
+<script type="ts">
+	import 'bytemd/dist/index.css'
+	import { Editor, Viewer } from 'bytemd'
+	import gfm from '@bytemd/plugin-gfm'
+  
+	let  value = ''
+	const plugins = [
+		gfm(),
+	  // Add more plugins here
+	]
+  
+	function handleChange(e) {
+	  value = e.detail.value
+	}
+
+	function salvar (e){
+		var p = document.querySelector('.markdown-body')
+		console.log('Salvar', p?.innerHTML)
+	}
+	
+	// <Editor {value} {plugins} on:change={handleChange} /> -->
+  </script>
+  
+  <div>	
+	<Editor {value} {plugins} on:change={handleChange} />
+	<div class="control">
+		<button on:click={salvar}>Salvar</button>
+	</div>
+  </div>
+
+  <style>
+	.control {
+		display: flex;
+		justify-content: end;
+	}
+  </style>
